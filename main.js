d8040a80fda3faf2f0ed46be260a885879b19431
Manuel-Smith <riddicksmith37@gmail.com>
2022-10-12 20:21:59 +0300

    Improving my javascript. Learned a bit

--- main.js
+++ main.js
@@ -1,28 +1,19 @@
-class Calculator {
-    constructor(){
-        let buttonOne = document.querySelector(".btn-1");
-        let buttonTwo = document.querySelector(".btn-2");
-        let buttonThree = document.querySelector(".btn-3");
-        let buttonFour = document.querySelector(".btn-4");
-        let buttonFive = document.querySelector(".btn-5");
-        let buttonSix = document.querySelector(".btn-6");
-        let buttonSeven = document.querySelector(".btn-7");
-        let buttonEight = document.querySelector(".btn-8");
-        let buttonNine = document.querySelector(".btn-9");
-        let buttonZero = document.querySelector(".btn-0");
-        let buttonAdd = document.querySelector(".btn-addition");
-        let buttonSubtract = document.querySelector(".btn-subtract");
-        let buttonMultiply = document.querySelector(".btn-multiply");
-        let buttonDivide = document.querySelector(".btn-divide");
-        let buttonEquals = document.querySelector(".btn-equals");
-        let buttonDelete = document.querySelector(".btn-de");
-        let buttonClear = document.querySelector(".btn-ac");
-        let buttonReciprical = document.querySelector(".btn-reciprical");
-        let buttonPercentage = document.querySelector(".btn-percentage");
-        let buttonDot = document.querySelector(".btn-dot");
-        let buttonChangeState = document.querySelector(".btn-state-change");
-        let buttonSquareRoot = document.querySelector(".btn-square-root");
-        let displayArea = document.querySelector(".display-area");
+// class Calculator {
+//     constructor(){
 
-    }
-}
+
+
+
+
+//     }    
+// }
+
+let displayArea = document.querySelector(".display2");
+
+let buttons = [...document.querySelectorAll('[data-number]')];
+
+buttons.forEach(button=>{
+    button.addEventListener('click', e=>{
+        displayArea.textContent += e.target.textContent;
+    })
+})
