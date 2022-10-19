d1f989da631e9f9f0542b54cc8160343e47a35c5
Manuel-Smith <riddicksmith37@gmail.com>
2022-10-19 18:06:39 +0300

    Added button to clear screen

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
 let numberButtons = [...document.querySelectorAll("[data-number]")];
 let displayArea2 = document.querySelector('.display2');
 let deleteButton = document.querySelector('[data-delete]')
+let clearButton = document.querySelector('[data-clear]')
 
 displayArea2.textContent = 0;
 
 
-
+// Functionality to check the Initial sum
 function initSum(){
     return [...displayArea2.textContent].map(value => Number(value)).reduce((Accum, InitialVal) => Accum += InitialVal, 0);
 }
+
 
 // Functionality to delete one item from the end
 deleteButton.addEventListener('click', event=>{
@@ -17,7 +19,11 @@ deleteButton.addEventListener('click', event=>{
     } else {
         displayArea2.textContent = displayArea2.textContent.slice(0, -1);
     }
-    console.log(initSum());
+})
+
+// Functionality to clear Everything from the screen
+clearButton.addEventListener('click', event=>{
+    displayArea2.textContent = 0;
 })
 
 // Functionality to display numbers on the page
