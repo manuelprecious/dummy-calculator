addee3fd4ffaf903924dda0797a90cf7dc6403b0
Manuel-Smith <riddicksmith37@gmail.com>
2022-10-19 18:00:35 +0300

    Added functionality to delete Item one by one

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 let numberButtons = [...document.querySelectorAll("[data-number]")];
 let displayArea2 = document.querySelector('.display2');
+let deleteButton = document.querySelector('[data-delete]')
 
 displayArea2.textContent = 0;
 
@@ -8,9 +9,18 @@ displayArea2.textContent = 0;
 function initSum(){
     return [...displayArea2.textContent].map(value => Number(value)).reduce((Accum, InitialVal) => Accum += InitialVal, 0);
 }
-console.log(typeof initSum());
 
+// Functionality to delete one item from the end
+deleteButton.addEventListener('click', event=>{
+    if(displayArea2.textContent.length <= 1){
+        displayArea2.textContent = 0;
+    } else {
+        displayArea2.textContent = displayArea2.textContent.slice(0, -1);
+    }
+    console.log(initSum());
+})
 
+// Functionality to display numbers on the page
 numberButtons.forEach(button => {
 
     button.addEventListener('click', event => {
@@ -18,11 +28,10 @@ numberButtons.forEach(button => {
             displayArea2.textContent += '.'
         } else if (event.target.textContent !== '.' && initSum() !== 0) {
             displayArea2.textContent += event.target.textContent;
-            console.log(initSum());
         } else if (event.target.textContent !== '.' && initSum() === 0) {
             displayArea2.textContent = event.target.textContent;
-            console.log(initSum())
         }
     })
 })
 
+console.log(deleteButton);
