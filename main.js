ceaefe1e34bf8dd078ddb979b62fb0e13e7293d1
Manuel-Smith <riddicksmith37@gmail.com>
2022-10-20 07:59:20 +0300

    Adding mini add and sub functionality

--- main.js
+++ main.js
@@ -1,21 +1,26 @@
 let numberButtons = [...document.querySelectorAll("[data-number]")];
+let displayArea1 = document.querySelector('.display1');
 let displayArea2 = document.querySelector('.display2');
 let deleteButton = document.querySelector('[data-delete]');
 let clearButton = document.querySelector('[data-clear]');
-let dataStateButton = document.querySelector('[data-state]');
-
+let buttonDataState = document.querySelector('[data-state]');
+let buttonAdd = document.querySelector('[data-add]');
+let buttonSubtract = document.querySelector('[data-subtract]');
+let buttonMultiply = document.querySelector('[data-multiply]');
+let buttonEquals = document.querySelector('[data-equals]');
 displayArea2.textContent = 0;
+let initialState = true;
 
 
 // Functionality to check the Initial sum
-function initSum(){
+function initSum() {
     return [...displayArea2.textContent].map(value => Number(value)).reduce((Accum, InitialVal) => Accum += InitialVal, 0);
 }
 
 
 // Functionality to delete one item from the end
-deleteButton.addEventListener('click', event=>{
-    if(displayArea2.textContent.length <= 1){
+deleteButton.addEventListener('click', event => {
+    if (displayArea2.textContent.length <= 1) {
         displayArea2.textContent = 0;
     } else {
         displayArea2.textContent = displayArea2.textContent.slice(0, -1);
@@ -23,10 +28,18 @@ deleteButton.addEventListener('click', event=>{
 })
 
 // Functionality to clear Everything from the screen
-clearButton.addEventListener('click', event=>{
-    displayArea2.textContent = 0;
+clearButton.addEventListener('click', event => {
+    clrAll();
 })
 
+function clrAll(){
+    displayArea1.textContent = 0;
+    displayArea2.textContent = 0;
+}
+
+function clr(){
+    displayArea2.textContent = 0;
+}
 // Functionality to display numbers on the page
 numberButtons.forEach(button => {
 
@@ -38,7 +51,38 @@ numberButtons.forEach(button => {
         } else if (event.target.textContent !== '.' && initSum() === 0) {
             displayArea2.textContent = event.target.textContent;
         }
-    })
-})
+    });
+});
 
-// Adding Functionality to the data state button
+// Function for Adding
+buttonAdd.addEventListener('click', event=>{
+    displayArea1.textContent = Number(displayArea1.textContent) + Number(displayArea2.textContent);
+    clr();
+});
+
+buttonSubtract.addEventListener('click', event=>{
+    displayArea1.textContent = Number(displayArea1.textContent) - Number(displayArea2.textContent);
+    clr();
+});
+
+buttonMultiply.addEventListener('click', event=>{
+    if(displayArea2.textContent.length === 1 && initSum() === 0){
+        
+    }
+    displayArea1.textContent = Number(displayArea1.textContent) * Number(displayArea2.textContent);
+    clr();
+});
+
+// //Function for subtracting
+// buttonSubtract.addEventListener('click', event=>{
+//     // displayArea1.textContent = Number(displayArea1.textContent) - Number(displayArea2.textContent);
+//     // clr();
+//     console.log('My hox')
+// });
+
+// Function Equals
+buttonEquals.addEventListener('click', event=>{
+    if(typeof displayArea2.textContent === 'string' || typeof displayArea1.textContent === 'number'){
+        displayArea1.textContent = Number(displayArea2.textContent);
+    };
+});
